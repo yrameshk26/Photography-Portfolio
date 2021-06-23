86ca5bab27c3de6f0f91f3d0cfb4ee503aa10cb0
Ramesh Yoha <yrameshk26@gmail.com>
2021-06-22 21:26:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ define('GRAV_PHP_MIN', '5.5.9');
 define('GRAV_SYSTEM_PATH', '/var/www/example.com/system');
 define('GRAV_CACHE_PATH', '/var/www/example.com/html');
 define('GRAV_LOG_PATH', '/var/www/example.com/html');
+define('GRAV_TMP_PATH', '/var/www/example.com/html');
+define('GRAV_BACKUP_PATH', '/var/www/example.com/html');
 
 // Ensure vendor libraries exist
 $autoload = __DIR__ . '/vendor/autoload.php';
